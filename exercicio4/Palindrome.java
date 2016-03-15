310956db0ebcd523c7bf42cd2c15ec9d7cb1ed6a
Bruna Tobias <bru-na-rafaela@hotmail.com>
2016-03-15 20:05:50 -0300

    Update Palindrome.java

--- exercicio4/Palindrome.java
+++ exercicio4/Palindrome.java
@@ -1,19 +1,19 @@
 public class Palindrome{
 
  public static void main(String[] args){
- String parametro = args[0];
- String final;
- String verificar = parametro;
- final = verificar;
- int tmnh, a, b, tp, posi1=0, posi2=0,teste;
- boolean i = false;
+    String parametro = args[0];
+    String final;
+    String verificar = parametro;
+    final = verificar;
+    int tmnh, a, b, tp, posi1=0, posi2=0,teste;
+    boolean i = false;
 	tp = verificar.length();
 	tmnh = verificar.length();
  while (tp > 1 && i == false){
 	a = tp;
 	for(posi1=0; posi2 <= posi2; posi1++){
-		b = posi1 + x;
-		verificar = final.substring(posi1,b);
+	b = posi1 + x;
+	verificar = final.substring(posi1,b);
 		
 	StringBuffer recebe = new StringBuffer(verificar);
 	recebe.reverse();
